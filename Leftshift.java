8ca6ec41e8d2486e360930af9acff42b4366e920
Chithra2936 <kurmachithrareddy2936@gmail.com>
2025-01-28 16:22:01 +0530

    Add files via upload

---
+++ Leftshift.java
@@ -0,0 +1,9 @@
+public class Leftshift {
+
+    public static void main(String[] args) {
+        int num = 5; 
+        int shiftedNum = num << 1; 
+        System.out.println("Original Number: " + num);
+        System.out.println("Shifted Number: " + shiftedNum);
+    }
+}
